b30165e8bd9baa9f0d6309cba55ca0a7e554c613
AndriiOshlakov <andreyoshlakov18051986@gmail.com>
2025-06-29 16:24:30 +0000

    Deploying to gh-pages from @ AndriiOshlakov/prctice_last_task@9be177478554e89830cef2746c66aee7a283f395 🚀

---
+++ wishlist.js
@@ -0,0 +1,2 @@
+import{S as t,r as e}from"./assets/refs-C_pHe5Cm.js";let o=JSON.parse(localStorage.getItem(t.WISH_KEY));e.itemsWishValue.textContent=o.length;const a=JSON.parse(localStorage.getItem(t.CARD_KEY))||[];e.totalProductsInCart.textContent=a.length;
+//# sourceMappingURL=wishlist.js.map
